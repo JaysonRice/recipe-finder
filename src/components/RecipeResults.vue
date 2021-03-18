4961c22075ca43afd5a6581c712b5b55220d283f
Jayson Rice <jayson.rice.95@gmail.com>
2021-03-18 11:26:07 -0400

    Deployed to firebase and recipe results prettied up.

--- src/components/RecipeResults.vue
+++ src/components/RecipeResults.vue
@@ -1,14 +1,25 @@
 <template>
   <v-row class="justify-space-around ma-2">
     <div v-for="recipe in searchResults" :key="recipe.id" class="ma-2">
-      <v-card width="300" height="100%">
+      <v-card width="350" height="100%">
         <v-img :src="recipe.image" width="350"></v-img>
-        <v-card-title>{{ recipe.title }}</v-card-title>
+        <v-card-title class="recipe-name">{{ recipe.title }}</v-card-title>
 
         <v-card-text>
-          <p>{{ recipe.readyInMinutes }} minutes</p>
-          <p>Serves {{ recipe.servings }}</p>
-          <a :href="recipe.sourceUrl" target="_blank">View Full Recipe</a>
+          <v-row class="mb-1 ">
+            <v-col md="4" cols="6">
+              <v-icon class="mr-1">mdi-clock</v-icon>
+              <span>{{ recipe.readyInMinutes }}min</span>
+            </v-col>
+            <v-col md="4" cols="6">
+              <v-icon class="mr-1">mdi-silverware</v-icon>
+              <span>Serves {{ recipe.servings }}</span></v-col
+            >
+          </v-row>
+          <div class="mb-2 subtitle-1">
+            <a :href="recipe.sourceUrl" target="_blank">View Full Recipe</a>
+          </div>
+
           <div><strong>Credits</strong>: {{ recipe.creditsText }}</div>
         </v-card-text>
       </v-card>
@@ -25,4 +36,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.recipe-name {
+  word-break: inherit;
+}
+</style>
